efc14ab3773c5c3595420478deee3e7b3ad9311a
Jasmine <jasminecharles@hotmail.com>
2019-09-30 12:57:01 -0700

    data

---
+++ src/components/TodoComponents/data.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const data = [
+    {
+  id: 0,
+  task: 'Organize Files',
+  completed: false
+
+    },
+    {
+    id: 1,
+    task: 'Learn Redux',
+    completed: false
+      
+    },
+    {
+    id: 2,
+    task: 'Create LinkedIn Profile',
+    completed: false
+          
+    },
+    {
+    id: 3,
+    task: 'Finish Portfolio',
+    completed: false
+      
+    },
+    {
+    id: 4,
+    task: 'Post on Twitter',
+    completed: false
+      
+    },
+    {
+    id: 5,
+    task: 'Do Yoga',
+    completed: false
+      
+    },
+]
